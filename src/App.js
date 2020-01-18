eb15c217a95f7469cfa5cf154043861f4a179db6
Jameson Hill <jhill@achieveit.com>
2020-01-18 12:05:14 -0500

    Add bio

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import './App.scss';
 function App() {
   return (
     <div className='App'>
+      <nav className='nav'>
+      </nav>
       <header>
         <img src={img} alt='profile' className='profilePic' />
+        <h2>Hi, I'm Jameson Hill</h2>
+        <div className='bio'>
+          <span className='item'>Husband</span>
+          <span className='item'>Father</span>
+          <span className='item'>Software Developer</span>
+        </div>
+        <div>Building delightful user experiences since 2016</div>
       </header>
     </div>
   );
